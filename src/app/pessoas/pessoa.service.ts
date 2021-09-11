6d185f0dc2a988ad4db3e5c27b75648cc8f2defc
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-09-10 22:37:03 -0400

    17.13. Desafio: implementando a exclusão de pessoas (correção)

--- src/app/pessoas/pessoa.service.ts
+++ src/app/pessoas/pessoa.service.ts
@@ -1,35 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
-import 'rxjs/add/operator/toPromise';
 export class PessoaFiltro {
   nome: string;
-  pagina= 0;
-  itensPorPagina = 5 ;
+  pagina = 0;
+  itensPorPagina = 5;
 }
 
 @Injectable()
-
 export class PessoaService {
 
   pessoasUrl = 'http://localhost:8080/pessoas';
 
   constructor(private http: HttpClient) { }
 
-  pesquisar(filtro: PessoaFiltro): Promise<any>{
+  pesquisar(filtro: PessoaFiltro): Promise<any> {
     const headers = new HttpHeaders()
       .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
-
     let params = new HttpParams()
       .set('page', filtro.pagina.toString())
       .set('size', filtro.itensPorPagina.toString());
 
     if (filtro.nome) {
-          params = params.set('nome', filtro.nome);
+      params = params.set('nome', filtro.nome);
     }
 
-
-    return this.http.get(`${this.pessoasUrl}?`, {headers, params})
+    return this.http.get(`${this.pessoasUrl}`, { headers, params })
       .toPromise()
       .then(response => {
         const pessoas = response['content'];
@@ -52,7 +48,7 @@ export class PessoaService {
       .then(response => response['content']);
   }
 
-  excluir(codigo: number): Promise<void>{
+  excluir(codigo: number): Promise<void> {
     const headers = new HttpHeaders()
       .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
 
