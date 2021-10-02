7da1fba595e70429398693aeb6941de893280583
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-10-02 12:24:21 -0400

    19.6 - Tratando casos de erros e sucesso de autenticação

--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { ErrorHandlerService } from './../../core/error-handler.service';
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,18 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent implements OnInit {
-  constructor(public auth: AuthService) {}
+  constructor(
+    public auth: AuthService,
+    private erroHandler: ErrorHandlerService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   login(usuario: string, senha: string) {
     this.auth.login(usuario, senha).subscribe(
       (response: any) => {
-        this.auth.armazenarToken(response.access_token);
         console.log(response);
+        this.auth.armazenarToken(response.access_token);
+        this.router.navigate(['lancamentos']);
       },
       (response: any) => {
         console.log(response);
+        let erro = response
+
+        if (response.status === 400){
+          if (response.error.error === 'invalid_grant'){
+            erro = 'Usuário ou Senha Inválida'
+          }
+        }
+        this.erroHandler.handle(erro);
       }
     );
   }
